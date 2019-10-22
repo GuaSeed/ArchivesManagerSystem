a7efab49b212e5b118ca0baf846a6fa59b3649e5
zzyandzzy <1428658308@qq.com>
2019-10-22 16:54:10 +0800

    add login page

--- src/main/java/com/guaseed/ams/controller/auth/AuthController.java
+++ src/main/java/com/guaseed/ams/controller/auth/AuthController.java
@@ -1,8 +1,16 @@
 package com.guaseed.ams.controller.auth;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @Author: intent
  * @Date: 19-10-22 下午3:58
  */
+@Controller
 public class AuthController {
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
 }
